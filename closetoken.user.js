baaef97e742ee220402690a3fa724c73cce6cad5
romariome <87151947+romariome@users.noreply.github.com>
2021-07-08 21:15:24 +0000

    Update closetoken.user.js

--- closetoken.user.js
+++ closetoken.user.js
@@ -9,23 +9,28 @@
 // @grant        none
 // ==/UserScript==
 
+var downlimit = 40;
+var uplimit = 200;
+
+
+
 var mrvlue = $(".mr-3");
 var val1 =mrvlue[0].innerText.replace(' addresses','');
 var val2 =val1.replace(/,/g,'');
 //alert('run');
-if(val2>200||val2<40)
+if(val2>downlimit && val2<uplimit)
 {
-    window.close();
-}
-else
-{
-   // $( "#overlayMain" ).remove();
+    // $( "#overlayMain" ).remove();
     var pathname = window.location.pathname;
     var tokenstr=pathname.slice(7,49);
     //alert(pathname);
     var varpoo ='https://poocoin.app/tokens/';
     var urlwinpoo =varpoo.concat(tokenstr);
     window.open(urlwinpoo, "_blank");
+}
+else
+{
+    window.close();
 }
 
 
